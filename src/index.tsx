17ea1b5cd77865becbc07dc2df7c4c8738085d5d
Bogdan-design <bogdanbw@gmail.com>
2023-07-17 12:45:34 +0200

    routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Root } from "routes/Root";
+import {SignIn} from "routes/SignIn";
 
-const container = document.getElementById('root')!;
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: "Error"
+  },
+  {
+    path: "sign-in",
+    element: <SignIn/>,
+    errorElement: "Error"
+  },
+
+]);
+
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <Provider store={store}>
       <App />
     </Provider>
