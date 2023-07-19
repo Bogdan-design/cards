2612d8fda0b00ca76d4f698eb0b33008e4193c54
Bogdan-design <bogdanbw@gmail.com>
2023-07-19 10:14:22 +0200

    auth-api

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import {appReducer} from "app/app.slice";
+import {authReducer} from "features/auth/auth.api";
 
 export const store = configureStore({
   reducer: {
     app: appReducer,
+    auth:authReducer,
     counter: counterReducer,
   },
 });
